797614e0da6e41d1cdd301d2373c2b9029dae420
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-06-04 15:20:35 +0200

    Added native navigation

--- App.js
+++ App.js
@@ -1,31 +1,45 @@
 import React from 'react';
-import { NavigationContainer, DefaultTheme  } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { enableScreens } from 'react-native-screens';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+import { HeaderBackButton } from '@react-navigation/stack';
 import { StyleSheet, Text, View } from 'react-native';
 import { ThemeManager } from 'react-native-ui-lib';
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
+import ChatRoom from './components/chat/ChatRoom';
 
-const Stack = createStackNavigator();
+enableScreens();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer theme={MyCodaTheme}>
-      <Stack.Navigator initialRouteName="login" 
-        screenOptions={{
-          headerStyle: {
-            elevation: 0,
-            shadowOpacity: 0
-          }
-        }}
-      >
-        <Stack.Screen name="register" component={Register} 
+      <Stack.Navigator 
+        initialRouteName="login"
+        screenOptions={{ headerHideShadow: true }}>
+        <Stack.Screen 
+          name="register" 
+          component={Register}
           options={{ headerShown: false }} />
-        <Stack.Screen name="login" component={Login} 
+        <Stack.Screen 
+          name="login" 
+          component={Login}
           options={{ headerShown: false }} />
-				<Stack.Screen name="home" component={Home} 
-          options={{ headerLeft: null, headerTitle: "MyCoda" }}/>
+        <Stack.Screen 
+          name="home" 
+          component={Home}
+          options={{ 
+            headerHideBackButton: true, 
+            headerTitle: "MyCoda" 
+          }}/>
+        <Stack.Screen
+          name="chat-room"
+          component={ChatRoom}
+          options={({ route }) => ({ 
+            title: route.params.name 
+          })} />
       </Stack.Navigator>
     </NavigationContainer>
   );
