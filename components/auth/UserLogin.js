87f70c570e5e63c8d13cf908488f0cad75352e05
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-07-01 22:08:39 +0200

    Added user login and register

--- components/auth/UserLogin.js
+++ components/auth/UserLogin.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { Alert } from 'react-native';
 import { View, TextField, Text, Button } from 'react-native-ui-lib';
+import { API } from '../../config/config';
+import * as SecureStore from 'expo-secure-store';
 
 export default class Login extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			cf: '',
+			fiscalCode: '',
 			password: '',
 		};
+	}
+
+	login() {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				cf: this.state.fiscalCode,
+				password: this.state.password,
+			})
+		};
+		fetch(`${API.URL}/api/users/login`, options)
+			.then((response) => response.json())
+			.then((json) => {
+				if ('error' in json) {
+					Alert.alert("Errore", "Credenziali errate");
+					return;
+				}
+				return SecureStore.setItemAsync('jwt', json.accessToken)
+			})
+			.then(() => { this.props.navigation.navigate('home'); })
+			.catch((error) => console.error(error))
 	}
 
 	render() {
@@ -18,17 +43,17 @@ export default class Login extends Component {
 				<Text red30 text60 marginB-10>Codice Fiscale</Text>
 				<TextField text70 dark10 
 					placeholder="Il tuo codice fiscale"
-					onChangeText={text => this.setState({cf : text})}
+					onChangeText={text => { this.state.fiscalCode = text }}
 				/>
 				<Text red30 text60 marginB-10>Password</Text>
 				<TextField text70 secureTextEntry dark10
 					placeholder="Password"
-					onChangeText={text => this.setState({ password: text })}
+					onChangeText={text => { this.state.password = text }}
 				/>
 				<View flex top>
 					<Button text70 white background-red30 borderRadius={10} marginT-10
 						label="Accedi"
-						onPress={() => this.props.navigation.navigate('home')}
+						onPress={() => this.login()}
 					/>
 					<Button link text70 red30 marginT-20
 						label="Registrati"
@@ -44,20 +69,4 @@ export default class Login extends Component {
 			</View>
 		);
 	}
-	/*
-	login() {
-
-		const options = {
-			method: 'POST',
-			body: JSON.stringify({
-				cf: this.state.cf,
-				password: this.state.password,
-			})
-		};
-		fetch(Constants.manifest.extra.API_URL, options)
-			.then((response) => response.json())
-			.then((json) => setData(json.movies))
-			.catch((error) => console.error(error))
-	}
-	*/
 }
--- components/auth/UserRegister.js
+++ components/auth/UserRegister.js
@@ -1,27 +1,78 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native'
+import { ScrollView, Alert } from 'react-native'
 import { View, TextField, Text, Button } from 'react-native-ui-lib';
+import { API } from '../../config/config';
 
 export default class Register extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			passwordConfirm: '',
+			name: '',
+			surname: '',
+			fiscalCode: '',
+			telephone: '',
+		};
+	}
+
+	register() {
+		const options = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				cf: this.state.fiscalCode,
+				nome: this.state.name,
+				cognome: this.state.surname,
+				numTel: this.state.telephone,
+				email: this.state.email,
+				password: this.state.password,
+			})
+		};
+		fetch(`${API.URL}/api/users/register`, options)
+			.then((response) => response.json())
+			.then((json) => {
+				if ('error' in json) {
+					Alert.alert("Errore", "Alcuni valori non sono validi");
+					return;
+				}
+				Alert.alert("Ottimo", "Registrazione avvenuta con successo");
+				this.props.navigation.navigate('user-login');
+			})
+			.catch((error) => console.error(error))
+	}
+
 	render() {
 		return (
 			<ScrollView>
 				<View flex paddingH-30 paddingT-30>
 					<Text grey20 text30 marginV-20>Registrati</Text>
 					<Text red30 text65>Email</Text>
-					<TextField text70 placeholder="La tua email" dark10 />
+					<TextField text70 placeholder="La tua email" dark10
+						onChangeText={(text) => this.state.email = text}/>
 					<Text red30 text65>Password</Text>
-					<TextField text70 placeholder="Password" secureTextEntry dark10 />
+					<TextField text70 placeholder="Password" secureTextEntry dark10 
+						onChangeText={(text) => this.state.password = text}/>
 					<Text red30 text65>Conferma Password</Text>
-					<TextField text70 placeholder="Reinserisci la tua password" secureTextEntry dark10 />
+					<TextField text70 placeholder="Reinserisci la tua password" secureTextEntry dark10 
+						onChangeText={(text) => this.state.passwordConfirm = text}/>
 					<Text red30 text65>Nome</Text>
-					<TextField text70 placeholder="Il tuo nome" dark10 />
+					<TextField text70 placeholder="Il tuo nome" dark10 
+						onChangeText={(text) => this.state.name = text}/>
 					<Text red30 text65>Cognome</Text>
-					<TextField text70 placeholder="Il tuo cognome" dark10 />
+					<TextField text70 placeholder="Il tuo cognome" dark10 
+						onChangeText={(text) => this.state.surname = text} />
 					<Text red30 text65>Codice Fiscale</Text>
-					<TextField text70 placeholder="Il tuo codice fiscale" dark10 />
+					<TextField text70 placeholder="Il tuo codice fiscale" dark10 
+						onChangeText={(text) => this.state.fiscalCode = text}/>
+					<Text red30 text65>Telefono</Text>
+					<TextField text70 placeholder="Il tuo numero di telefono" dark10
+						onChangeText={(text) => this.state.telephone = text} />
 					<View flex top>
-						<Button text70 white background-red30 label="Continua" borderRadius={10} marginT-10 />
+						<Button text70 white background-red30 label="Continua" borderRadius={10} marginT-10 
+							onPress={() => this.register()}/>
 						<View flex row top marginB-30 flex centerH marginT-20>
 							<Text grey10 text70 centerV>Hai già un account? </Text>
 							<Button link text70 red30
