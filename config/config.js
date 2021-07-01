088eac5ce045743920af03e2f34246bd9f1b7926
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-07-01 22:08:49 +0200

    Added config for endpoints

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const API = {
+  URL: 'https://api.server.mycoda.it' || '192.168.1.100',
+};
+
+const REALTIME = {
+  HOST: '192.168.1.100',
+  PORT: '8080',
+};
+
+export {
+  API,
+  REALTIME,
+};
