827c112e17855e93c820cd83cadfbff655480893
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-07-04 20:45:28 +0200

    Added pharmacies fetch from api

--- components/pharmacies/Pharmacies.js
+++ components/pharmacies/Pharmacies.js
@@ -1,32 +1,50 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { FlatList } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
+import { API } from '../../config/config';
 import PharmacyItem from './PharmacyItem';
 
 export default class Pharmacies extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { pharmacies: [], refreshing: false };
+  }
+
+  componentDidMount() {
+    this.fetchPharmacies();
+  }
+
+  fetchPharmacies() {
+    this.setState({ refreshing: true });
+    SecureStore.getItemAsync('jwt')
+      .then(jwt => {
+        return fetch(`${API.URL}/api/pharmacies`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`,
+          },
+        });
+      })
+      .then(response => response.json())
+      .then(json => { this.setState({ pharmacies: json, refreshing: false }); })
+      .catch(error => console.error(error))
   }
 
   render() {
     return (
-      <ScrollView>
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-        <PharmacyItem name="Farmacia Vitti" address="Via Corato, Andria" imgUri="https://reactnative.dev/img/tiny_logo.png" />
-      </ScrollView>
+      <FlatList
+        data={this.state.pharmacies}
+        keyExtractor={item => item.pIva}
+        onRefresh={() => this.fetchPharmacies()}
+        refreshing={this.state.refreshing}
+        renderItem={({ item }) => {
+          return <PharmacyItem
+            navigation={this.props.navigation}
+            name={item.ragSociale}
+            address={item.indirizzo}
+            imgUri="https://reactnative.dev/img/tiny_logo.png"/>
+        }}/>
     );
   }
 }
