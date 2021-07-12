c2413b3f82de8e11ede923ed011fd629ea77fbf5
ArciducaZagaria <pierluigizagaria@gmail.com>
2021-07-13 00:16:40 +0200

    Added jwt token to socket connection

--- components/SocketContext.js
+++ components/SocketContext.js
@@ -2,14 +2,30 @@
 import React, { Component } from 'react';
 import io from "socket.io-client";
 import { REALTIME } from '../config/config';
+import userData from '../helpers/userData';
 
 export const SocketContext = React.createContext();
 
 export class SocketContextProvider extends Component {
   socket = io(`${REALTIME.HOST}:${REALTIME.PORT}`, { jsonp: false });
+  constructor(props) {
+    super(props);
+    this.state = { socket: null };
+    userData.load().then((data) => 
+      this.setState({
+        socket: io(`${REALTIME.HOST}:${REALTIME.PORT}`, {
+          query: {
+            authentication: `Bearer ${data.accessToken}`,
+          },
+          jsonp: false
+        })
+      })
+    );
+  }
+
   render() {
     return (
-      <SocketContext.Provider value={this.socket}>
+      <SocketContext.Provider value={this.state.socket}>
         {this.props.children}
       </SocketContext.Provider>
     );
